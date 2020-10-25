fba099ba947e167b966821b68ff2595c83ef92a5
MNabawy <m_ninja61@yahoo.com>
2020-10-25 19:32:52 +0200

    adding schema

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 
 
 // connect mongoose
-const uri = 'mongodb://test:admin123@guestbook-shard-00-00.3saiw.mongodb.net:27017,guestbook-shard-00-01.3saiw.mongodb.net:27017,guestbook-shard-00-02.3saiw.mongodb.net:27017/guestbook?ssl=true&replicaSet=atlas-iw9fj0-shard-0&authSource=admin&retryWrites=true&w=majority'
+const uri = require('./src/config/index').DbUri
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology:true })
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('db connected'))
-  .catch (err => console.error(err))
+  .catch(err => console.error(err))
 
 const app = express()
 const port = 3000
---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+module.exports ={
+    DbUri : 'mongodb://test:admin123@guestbook-shard-00-00.3saiw.mongodb.net:27017,guestbook-shard-00-01.3saiw.mongodb.net:27017,guestbook-shard-00-02.3saiw.mongodb.net:27017/guestbook?ssl=true&replicaSet=atlas-iw9fj0-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
