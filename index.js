35b5c462dd05fe25853c398475271dd6a9bab65b
MNabawy <m_ninja61@yahoo.com>
2020-10-26 17:55:01 +0200

    user model

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const path = require('path');
+// configure mongoose's promise to global promise
+mongoose.Promise = global.Promise
 
+// configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
 
+// initiate our app
+const app = express()
+
+// configure our app
+
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'app secret', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if (!isProduction) {
+  app.use(errorHandler());
+}
+
+// Error handlers & middlewares
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      }
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    }
+  })
+})
 
 // connect mongoose
 const uri = require('./src/config/index').DbUri
@@ -9,8 +58,11 @@ const uri = require('./src/config/index').DbUri
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('db connected'))
   .catch(err => console.error(err))
+mongoose.set('debug', true);
 
-const app = express()
+// models & routes
+require('./src/models/user');
+
 const port = 3000
 
 app.get('/', (req, res) => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,49 @@
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    email:String,
+    email: String,
     password: String,
     consfirmPassword: String
-})
+});
 
-const User = mongoose.model('User' , userSchema);
+UserSchema.methods.setPassword = function (inputPassword) {
+    this.password = crypto.randomBytes(16).toString('hex');
+    this.consfirmPassword = crypto.pbkdf2Sync(password, this.inputPassword, 1000, 512, 'sha512').toString('hex');
+}
+
+
+UserSchema.methods.validatePassword = function (inputPassword) {
+    const password = crypto.pbkdf2Sync(password, this.consfirmPassword, 1000, 512, 'sha512').toString('hex');
+    return this.consfirmPassword === password;
+}
+
+UserSchema.methods.generatJWT = function(){
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        email:this.email,
+        id:this._id,
+        exp:parseInt(expirationDate.getTime() / 1000, 10),
+
+    },'secret');
+} 
+
+UserSchema.methods.toAuthJSON = function() {
+    return {
+        _id: this._id,
+        password: this.password,
+        consfirmPassword:this.consfirmPassword
+    }
+}
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
